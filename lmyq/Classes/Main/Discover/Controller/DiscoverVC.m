1f3dced854f6d7239414bb18a4dcd78b2f680588
Leo <leoios@sina.com>
2015-11-25 14:46:59 +0800

    1. 设置发现页和设置页的背景图片

--- lmyq/Classes/Main/Discover/Controller/DiscoverVC.m
+++ lmyq/Classes/Main/Discover/Controller/DiscoverVC.m
@@ -18,7 +18,33 @@
     
     [super viewDidLoad];
     
+    UIImageView *bgView = [[UIImageView alloc] init];
+    bgView.image = [UIImage imageNamed:@"bg"];
+    self.tableView.backgroundView = bgView;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     
+    return section == 0 ? 36.0f : 20.0f;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    
+    return 16.0f;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    
+    UIView *headerView = [[UIView alloc] init];
+    headerView.backgroundColor = [UIColor clearColor];
+    return headerView;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    
+    UIView *footerView = [[UIView alloc] init];
+    footerView.backgroundColor = [UIColor clearColor];
+    return footerView;
 }
 
 @end
