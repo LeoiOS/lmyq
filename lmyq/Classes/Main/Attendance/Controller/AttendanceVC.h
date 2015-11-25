9431273e8ff066ccaaa159bb4acf32071485976c
Leo <leoios@sina.com>
2015-11-25 11:40:17 +0800

    1. 新加打卡、联系人、发现、设置界面

---
+++ lmyq/Classes/Main/Attendance/Controller/AttendanceVC.h
@@ -0,0 +1,13 @@
+//
+//  AttendanceVC.h
+//  lmyq
+//
+//  Created by Leo on 15/11/25.
+//  Copyright © 2015年 LanMeng Tec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttendanceVC : UIViewController
+
+@end
---
+++ lmyq/Classes/Main/Setting/Controller/SettingVC.m
@@ -0,0 +1,37 @@
+//
+//  SettingVC.m
+//  lmyq
+//
+//  Created by Leo on 15/11/25.
+//  Copyright © 2015年 LanMeng Tec. All rights reserved.
+//
+
+#import "SettingVC.h"
+
+@interface SettingVC ()
+
+@end
+
+@implementation SettingVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
